684d3ad550b05052447ea2a277551e77595b0791
Lee Hopkins <leerhop@gmail.com>
2021-12-02 19:47:02 -0500

    so this does work after all, still not sure why tho

--- DayTwo.exs
+++ DayTwo.exs
@@ -1,22 +1,35 @@
 partOneResult = File.stream!("/mnt/c/Users/lee-h/source/repos/AoC2021.iex/DayTwoInput.txt")
 |> Stream.map(fn(line) ->
-    # I tried a couple different iterations of splitting and parsing, this has produced the most consistent results
-    splits = String.split(String.trim(line));
-    # originally I was not trimming, parsing, and transposing to a tuple and instead just using the
-    # list returned by String.split, but that was causing some access issues in the reduce below
+    splits = String.split(String.trim(line))
     {Enum.at(splits, 0), Integer.parse(Enum.at(splits, 1))}
 end)
 |> Enum.to_list()
 |> Enum.reduce({0, 0}, fn(cmd, {depth, horizontal}) ->
     case cmd do
-        # x here is acting like a tuple and returning {2, ""} and I can't figure out why
-        # If I call is_tuple(x) it returns false
-        # if I try elem(x, 0) it says its not a tuple and throws an exception
-        {"forward", x} -> {depth, horizontal + x}
-        {"down", x} -> {depth + x, horizontal}
-        {"up", x} -> {depth - x, horizontal}
+        # so treating x as tuple does work, I'm not sure why it wasn't before (probably user error)
+        # but really I have no idea why x is tuple here...
+        {"forward", x} -> {depth, horizontal + elem(x, 0)}
+        {"down", x} -> {depth + elem(x, 0), horizontal}
+        {"up", x} -> {depth - elem(x, 0), horizontal}
         _ -> {depth, horizontal}
     end
 end)
 
 IO.puts(elem(partOneResult, 0) * elem(partOneResult, 1))
+
+partTwoResult = File.stream!("/mnt/c/Users/lee-h/source/repos/AoC2021.iex/DayTwoInput.txt")
+|> Stream.map(fn(line) ->
+    splits = String.split(String.trim(line))
+    {Enum.at(splits, 0), Integer.parse(Enum.at(splits, 1))}
+end)
+|> Enum.to_list()
+|> Enum.reduce({0, 0, 0}, fn(cmd, {depth, horizontal, aim}) ->
+    case cmd do
+        {"forward", x} -> {depth + aim * elem(x, 0), horizontal + elem(x, 0), aim}
+        {"down", x} -> {depth, horizontal, aim + elem(x, 0)}
+        {"up", x} -> {depth, horizontal, aim -  elem(x, 0)}
+        _ -> {depth, horizontal}
+    end
+end)
+
+IO.puts(elem(partTwoResult, 0) * elem(partTwoResult, 1))

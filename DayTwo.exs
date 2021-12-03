73d577d4f2d18c5d01158220ad0e2f6d0c5a946a
Lee Hopkins <leerhop@gmail.com>
2021-12-02 20:08:51 -0500

    this is better

--- DayTwo.exs
+++ DayTwo.exs
@@ -1,16 +1,14 @@
 partOneResult = File.stream!("/mnt/c/Users/lee-h/source/repos/AoC2021.iex/DayTwoInput.txt")
 |> Stream.map(fn(line) ->
     splits = String.split(String.trim(line))
-    {Enum.at(splits, 0), Integer.parse(Enum.at(splits, 1))}
+    {Enum.at(splits, 0), elem(Integer.parse(Enum.at(splits, 1)), 0)}
 end)
 |> Enum.to_list()
 |> Enum.reduce({0, 0}, fn(cmd, {depth, horizontal}) ->
     case cmd do
-        # so treating x as tuple does work, I'm not sure why it wasn't before (probably user error)
-        # but really I have no idea why x is tuple here...
-        {"forward", x} -> {depth, horizontal + elem(x, 0)}
-        {"down", x} -> {depth + elem(x, 0), horizontal}
-        {"up", x} -> {depth - elem(x, 0), horizontal}
+        {"forward", x} -> {depth, horizontal + x}
+        {"down", x} -> {depth + x, horizontal}
+        {"up", x} -> {depth - x, horizontal}
         _ -> {depth, horizontal}
     end
 end)
@@ -20,14 +18,14 @@ IO.puts(elem(partOneResult, 0) * elem(partOneResult, 1))
 partTwoResult = File.stream!("/mnt/c/Users/lee-h/source/repos/AoC2021.iex/DayTwoInput.txt")
 |> Stream.map(fn(line) ->
     splits = String.split(String.trim(line))
-    {Enum.at(splits, 0), Integer.parse(Enum.at(splits, 1))}
+    {Enum.at(splits, 0), elem(Integer.parse(Enum.at(splits, 1)), 0)}
 end)
 |> Enum.to_list()
 |> Enum.reduce({0, 0, 0}, fn(cmd, {depth, horizontal, aim}) ->
     case cmd do
-        {"forward", x} -> {depth + aim * elem(x, 0), horizontal + elem(x, 0), aim}
-        {"down", x} -> {depth, horizontal, aim + elem(x, 0)}
-        {"up", x} -> {depth, horizontal, aim -  elem(x, 0)}
+        {"forward", x} -> {depth + aim * x, horizontal + x, aim}
+        {"down", x} -> {depth, horizontal, aim + x}
+        {"up", x} -> {depth, horizontal, aim - x}
         _ -> {depth, horizontal, aim}
     end
 end)
